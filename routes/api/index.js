ecaff6072072a041192f27d230b1ef2e0c594848
Alba Cardona <alba.cardonajimenez@gmail.com>
2021-11-24 14:27:31 +0100

    Renamed user.routes.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/auth', require('./auth.routes'));
+router.use('/user', require('./user.routes'));
 router.use('/comments', require('./comments.routes'));
 
 module.exports = router;
